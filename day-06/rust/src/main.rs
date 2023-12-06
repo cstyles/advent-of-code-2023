785610c1177fc48730ee7782ff4dd2e24beededc
Collin Styles <collingstyles@gmail.com>
2023-12-06 08:49:49 -0800

    Day 06, Rust: Algebraic approach

--- day-06/rust/src/main.rs
+++ day-06/rust/src/main.rs
@@ -13,8 +13,7 @@ fn main() {
         .product();
     println!("part1 = {part1}");
 
-    // println!("part2 = {}", attempt(71530, 940200));
-    println!("part2 = {}", attempt(41667266, 244104712281040));
+    println!("part2 = {}", quadratic_formula(41667266.0, 244104712281040.0));
 }
 
 fn attempt(round_length: u64, record: u64) -> usize {
@@ -22,4 +21,19 @@ fn attempt(round_length: u64, record: u64) -> usize {
         .map(|speed| speed * (round_length - speed))
         .filter(|distance| *distance > record)
         .count()
+}
+
+// record = time_held * (round_length - time_held)
+// record = time_held * round_length - time_held^2
+// 0 = time_held * round_length - time_held^2 - record
+// 0 = time_held^2 - time_held * round_length + record
+//
+// a = 1, b = -round_length, c = record
+// x = (round_length ± sqrt(round_length^2 - 4 * record)) / 2
+fn quadratic_formula(round_length: f64, record: f64) -> f64 {
+    let discriminant = round_length.powf(2.0) - 4.0 * record;
+    let upper = round_length + discriminant.sqrt() / 2.0;
+    let lower = round_length - discriminant.sqrt() / 2.0;
+
+    upper.floor() - lower.floor()
 }
