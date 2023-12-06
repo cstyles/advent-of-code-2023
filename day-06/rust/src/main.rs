77b5f3d2a3d82fa81ae818ba6d482fdbb20464cc
Collin Styles <collingstyles@gmail.com>
2023-12-05 21:10:13 -0800

    Day 06, Rust: Part 2

--- day-06/rust/src/main.rs
+++ day-06/rust/src/main.rs
@@ -1,10 +1,10 @@
 fn main() {
     // let input = include_str!("../../input.txt");
-    let times = [41, 66, 72, 66];
-    let distances = [244, 1047, 1228, 1040];
+    // let times = [41, 66, 72, 66];
+    // let distances = [244, 1047, 1228, 1040];
 
-    // let times = [7, 15, 30];
-    // let distances = [9, 40, 200];
+    let times = [7, 15, 30];
+    let distances = [9, 40, 200];
 
     let part1: usize = times
         .into_iter()
@@ -12,6 +12,9 @@ fn main() {
         .map(|(round_length, record)| attempt(round_length, record))
         .product();
     println!("part1 = {part1}");
+
+    // println!("part2 = {}", attempt(71530, 940200));
+    println!("part2 = {}", attempt(41667266, 244104712281040));
 }
 
 fn attempt(round_length: u64, record: u64) -> usize {
