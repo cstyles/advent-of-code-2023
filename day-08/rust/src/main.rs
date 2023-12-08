47ca13829e54536e98d25dd28a695b5f8e8aa760
Collin Styles <collingstyles@gmail.com>
2023-12-07 23:58:22 -0800

    Day 08, Rust: Part 2

--- day-08/rust/src/main.rs
+++ day-08/rust/src/main.rs
@@ -1,4 +1,4 @@
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet};
 
 #[derive(Debug, Clone, Copy)]
 enum Direction {
@@ -19,6 +19,7 @@ impl Direction {
 fn main() {
     // let input = include_str!("../../test_input.txt");
     // let input = include_str!("../../test_input2.txt");
+    // let input = include_str!("../../test_input3.txt");
     let input = include_str!("../../input.txt");
 
     let (lrs, graph) = input.split_once("\n\n").unwrap();
@@ -35,10 +36,8 @@ fn main() {
         })
         .collect();
 
-    // dbg!(lrs);
-    // dbg!(graph);
-
-    part1(lrs.into_iter().cycle(), graph);
+    part1(lrs.clone().into_iter().cycle(), graph.clone());
+    part2(lrs.into_iter().enumerate().cycle(), graph);
 }
 
 type Graph<'a> = HashMap<&'a str, (&'a str, &'a str)>;
@@ -57,4 +56,76 @@ fn part1<I: Iterator<Item = Direction>>(mut directions: I, graph: Graph) {
     }
 
     println!("part1 = {i}");
+}
+
+fn part2<I: Iterator<Item = (usize, Direction)>>(mut directions: I, graph: Graph) {
+    let mut i = 0;
+    let mut nodes: Vec<&str> = graph
+        .keys()
+        .copied()
+        .filter(|node| node.ends_with('A'))
+        .collect();
+
+    // Positions that we've already seen
+    // Keep track of the position and where we were through the direction iter
+    let mut seen: Vec<HashSet<(&str, usize)>> = nodes
+        .iter()
+        .copied()
+        .map(|node| HashSet::from([(node, 0)]))
+        .collect();
+
+    // What index we saw the first loop at
+    let mut seen_indices = vec![None; nodes.len()];
+
+    while !nodes.iter().all(|node| node.ends_with('Z')) {
+        let (di, direction) = directions.next().unwrap();
+
+        for node in nodes.iter_mut() {
+            let (left, right) = graph.get(node).unwrap();
+            *node = match direction {
+                Direction::Left => left,
+                Direction::Right => right,
+            }
+        }
+
+        // For every path we're taking through
+        for (index, node) in nodes.iter().copied().enumerate() {
+            // Skip if we've already figured out how often this node loops
+            if seen_indices[index].is_some() {
+                continue;
+            }
+
+            // Check if we've looped
+            if !seen.get_mut(index).unwrap().insert((node, di)) {
+                // Record the minimum times needed to loop
+                seen_indices[index].get_or_insert(i);
+            }
+        }
+
+        if seen_indices.iter().all(Option::is_some) {
+            break;
+        }
+
+        i += 1;
+    }
+
+    let diffs = seen_indices
+        .into_iter()
+        .map(|index| index.unwrap_or(i))
+        .map(|index| index - index % 263);
+
+    let part2 = diffs.fold(1, lcm);
+
+    println!("part2 = {part2}");
+}
+
+fn lcm(a: u64, b: u64) -> u64 {
+    (a * b) / gcd(a, b)
+}
+
+fn gcd(a: u64, b: u64) -> u64 {
+    match b {
+        0 => a,
+        _ => gcd(b, a % b),
+    }
 }
