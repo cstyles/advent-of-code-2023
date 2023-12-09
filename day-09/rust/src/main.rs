4425b00a2460ebd18e1f36e16f667ef586966c2f
Collin Styles <collingstyles@gmail.com>
2023-12-08 21:45:23 -0800

    Day 09, Rust: Merge solutions into a single function

--- day-09/rust/src/main.rs
+++ day-09/rust/src/main.rs
@@ -5,30 +5,21 @@ fn main() {
     let (part1, part2) = input
         .lines()
         .map(parse_row)
-        .map(|numbers| (solve(&numbers), solve2(&numbers)))
+        .map(|numbers| (solve::<false>(&numbers), solve::<true>(&numbers)))
         .fold((0, 0), sum2);
 
     println!("part1 = {part1}");
     println!("part2 = {part2}");
 }
 
-fn solve(row: &[i64]) -> i64 {
+fn solve<const PART_TWO: bool>(row: &[i64]) -> i64 {
     let next_row: Vec<_> = row.windows(2).map(|nums| nums[1] - nums[0]).collect();
 
-    if next_row.iter().all(|num| *num == 0) {
-        *row.last().unwrap()
-    } else {
-        row.last().unwrap() + solve(&next_row)
-    }
-}
-
-fn solve2(row: &[i64]) -> i64 {
-    let next_row: Vec<_> = row.windows(2).map(|nums| nums[1] - nums[0]).collect();
-
-    if next_row.iter().all(|num| *num == 0) {
-        *row.first().unwrap()
-    } else {
-        row.first().unwrap() - solve2(&next_row)
+    match (PART_TWO, next_row.iter().all(|num| *num == 0)) {
+        (false, false) => row.last().unwrap() + solve::<PART_TWO>(&next_row),
+        (false, true) => *row.last().unwrap(),
+        (true, false) => row.first().unwrap() - solve::<PART_TWO>(&next_row),
+        (true, true) => *row.first().unwrap(),
     }
 }
 
