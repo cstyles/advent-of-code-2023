2e2061dc7cb8e16eac22609055a675fe9bbd14d3
Collin Styles <collingstyles@gmail.com>
2023-12-08 21:56:09 -0800

    Day 09, Rust: Calculate part2 by just reversing the list

--- day-09/rust/src/main.rs
+++ day-09/rust/src/main.rs
@@ -1,25 +1,21 @@
 fn main() {
     // let input = include_str!("../../test_input.txt");
     let input = include_str!("../../input.txt");
+    let rows: Vec<_> = input.lines().map(parse_row).collect();
 
-    let (part1, part2) = input
-        .lines()
-        .map(parse_row)
-        .map(|numbers| (solve::<false>(&numbers), solve::<true>(&numbers)))
-        .fold((0, 0), sum2);
+    let part1: i64 = rows.iter().map(AsRef::as_ref).map(solve).sum();
+    let part2: i64 = rows.into_iter().map(reverse).map(|nums| solve(&nums)).sum();
 
     println!("part1 = {part1}");
     println!("part2 = {part2}");
 }
 
-fn solve<const PART_TWO: bool>(row: &[i64]) -> i64 {
+fn solve(row: &[i64]) -> i64 {
     let next_row: Vec<_> = row.windows(2).map(|nums| nums[1] - nums[0]).collect();
 
-    match (PART_TWO, next_row.iter().all(|num| *num == 0)) {
-        (false, false) => row.last().unwrap() + solve::<PART_TWO>(&next_row),
-        (false, true) => *row.last().unwrap(),
-        (true, false) => row.first().unwrap() - solve::<PART_TWO>(&next_row),
-        (true, true) => *row.first().unwrap(),
+    match next_row.iter().all(|num| *num == 0) {
+        false => row.last().unwrap() + solve(&next_row),
+        true => *row.last().unwrap(),
     }
 }
 
@@ -27,6 +23,7 @@ fn parse_row(line: &str) -> Vec<i64> {
     line.split(' ').map(|num| num.parse().unwrap()).collect()
 }
 
-fn sum2((a, b): (i64, i64), (c, d): (i64, i64)) -> (i64, i64) {
-    (a + c, b + d)
+fn reverse<T>(mut v: Vec<T>) -> Vec<T> {
+    v.reverse();
+    v
 }
