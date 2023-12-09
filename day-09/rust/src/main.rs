e6c5b81272f4d86334e24529ddeb47547bc39ac2
Collin Styles <collingstyles@gmail.com>
2023-12-08 21:24:05 -0800

    Day 09, Rust: Part 2

--- day-09/rust/src/main.rs
+++ day-09/rust/src/main.rs
@@ -4,6 +4,9 @@ fn main() {
 
     let part1: i64 = input.lines().map(part1).sum();
     println!("part1 = {part1}");
+
+    let part2: i64 = input.lines().map(part2).sum();
+    println!("part2 = {part2}");
 }
 
 fn part1(line: &str) -> i64 {
@@ -19,5 +22,21 @@ fn solve(row: Vec<i64>) -> i64 {
         *row.last().unwrap()
     } else {
         row.last().unwrap() + solve(next_row)
+    }
+}
+
+fn part2(line: &str) -> i64 {
+    let numbers: Vec<i64> = line.split(' ').map(|n| n.parse().unwrap()).collect();
+
+    solve2(numbers)
+}
+
+fn solve2(row: Vec<i64>) -> i64 {
+    let next_row: Vec<_> = row.windows(2).map(|nums| nums[1] - nums[0]).collect();
+
+    if next_row.iter().all(|num| *num == 0) {
+        *row.first().unwrap()
+    } else {
+        row.first().unwrap() - solve2(next_row)
     }
 }
