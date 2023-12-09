c15403fd239362e8dc00955d816aea2f6491b374
Collin Styles <collingstyles@gmail.com>
2023-12-08 21:41:06 -0800

    Day 09, Rust: Clean up

--- day-09/rust/src/main.rs
+++ day-09/rust/src/main.rs
@@ -2,41 +2,40 @@ fn main() {
     // let input = include_str!("../../test_input.txt");
     let input = include_str!("../../input.txt");
 
-    let part1: i64 = input.lines().map(part1).sum();
-    println!("part1 = {part1}");
+    let (part1, part2) = input
+        .lines()
+        .map(parse_row)
+        .map(|numbers| (solve(&numbers), solve2(&numbers)))
+        .fold((0, 0), sum2);
 
-    let part2: i64 = input.lines().map(part2).sum();
+    println!("part1 = {part1}");
     println!("part2 = {part2}");
 }
 
-fn part1(line: &str) -> i64 {
-    let numbers: Vec<i64> = line.split(' ').map(|n| n.parse().unwrap()).collect();
-
-    solve(numbers)
-}
-
-fn solve(row: Vec<i64>) -> i64 {
+fn solve(row: &[i64]) -> i64 {
     let next_row: Vec<_> = row.windows(2).map(|nums| nums[1] - nums[0]).collect();
 
     if next_row.iter().all(|num| *num == 0) {
         *row.last().unwrap()
     } else {
-        row.last().unwrap() + solve(next_row)
+        row.last().unwrap() + solve(&next_row)
     }
 }
 
-fn part2(line: &str) -> i64 {
-    let numbers: Vec<i64> = line.split(' ').map(|n| n.parse().unwrap()).collect();
-
-    solve2(numbers)
-}
-
-fn solve2(row: Vec<i64>) -> i64 {
+fn solve2(row: &[i64]) -> i64 {
     let next_row: Vec<_> = row.windows(2).map(|nums| nums[1] - nums[0]).collect();
 
     if next_row.iter().all(|num| *num == 0) {
         *row.first().unwrap()
     } else {
-        row.first().unwrap() - solve2(next_row)
+        row.first().unwrap() - solve2(&next_row)
     }
+}
+
+fn parse_row(line: &str) -> Vec<i64> {
+    line.split(' ').map(|num| num.parse().unwrap()).collect()
+}
+
+fn sum2((a, b): (i64, i64), (c, d): (i64, i64)) -> (i64, i64) {
+    (a + c, b + d)
 }
