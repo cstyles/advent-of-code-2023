fa8691698ab53a2ae494d741e98cffca1321c716
Collin Styles <collingstyles@gmail.com>
2023-12-12 11:48:21 -0800

    Day 12, Rust: Part 2

--- day-12/rust/src/main.rs
+++ day-12/rust/src/main.rs
@@ -1,4 +1,6 @@
-#[derive(Debug, Copy, Clone, PartialEq, Eq)]
+use std::collections::HashMap;
+
+#[derive(Debug, Copy, Clone, PartialEq, Eq, Hash)]
 enum Status {
     Operational,
     Damaged,
@@ -33,7 +35,7 @@ struct Row {
 impl std::fmt::Debug for Row {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         let row: String = self.springs.iter().map(Status::as_char).collect();
-        write!(f, "{row}")
+        write!(f, "{row} {:?}", self.groups)
     }
 }
 
@@ -46,81 +48,125 @@ impl Row {
         Self { springs, groups }
     }
 
-    fn possible_arrangements(self) -> usize {
-        let unknown = self
-            .springs
-            .iter()
-            .enumerate()
-            .find(|(_, status)| **status == Status::Unknown)
-            .map(|(i, _)| i);
-
-        match unknown {
-            Some(position) => [Status::Operational, Status::Damaged]
-                .into_iter()
-                .map(|status| self.replace(position, status))
-                .map(Row::possible_arrangements)
-                .sum(),
-            None => {
-                if self.is_valid() {
-                    1
-                } else {
-                    0
-                }
-            }
-        }
-    }
-
-    fn is_valid(&self) -> bool {
-        let mut group_sizes = vec![];
-        let mut springs = self.springs.iter();
-        let spring = springs.next().unwrap();
-        let mut in_group = *spring == Status::Damaged;
-        let mut group_size = usize::from(in_group);
-
-        for spring in springs {
-            match spring {
-                Status::Operational => {
-                    if in_group {
-                        group_sizes.push(group_size);
-                    }
-
-                    in_group = false;
-                    group_size = 0;
-                }
-                Status::Damaged => {
-                    in_group = true;
-                    group_size += 1;
-                }
-                Status::Unknown => todo!(),
-            }
-        }
-
-        if in_group {
-            group_sizes.push(group_size);
-        }
-
-        if group_sizes.len() != self.groups.len() {
-            return false;
-        }
-
-        group_sizes
-            .into_iter()
-            .zip(self.groups.iter())
-            .all(|(a, b)| a == *b)
-    }
-
     fn replace(&self, position: usize, status: Status) -> Self {
-        let mut this = self.clone();
-        this.springs[position] = status;
-        this
+        self.clone().replace_owned(position, status)
+    }
+
+    fn replace_owned(mut self, position: usize, status: Status) -> Self {
+        self.springs[position] = status;
+        self
+    }
+
+    fn unfold(self) -> Self {
+        let springs_length = self.springs.len();
+        let mut springs = self.springs;
+        springs.push(Status::Unknown);
+
+        let mut springs: Vec<Status> = springs
+            .into_iter()
+            .cycle()
+            .take(5 * (springs_length + 1))
+            .collect();
+        let _ = springs.pop();
+
+        let groups_length = self.groups.len();
+        let groups = self
+            .groups
+            .into_iter()
+            .cycle()
+            .take(5 * groups_length)
+            .collect();
+
+        Self { springs, groups }
+    }
+
+    fn possible_arrangements(mut self, cache: &mut Cache) -> u64 {
+        if let Some(num) = cache.get(&(self.springs.clone(), self.groups.clone())) {
+            return *num;
+        }
+
+        match (self.springs.first(), self.groups.first().copied()) {
+            (None, None) => 1,
+            (None, Some(_)) => 0,               // invalid
+            (Some(Status::Damaged), None) => 0, // invalid
+            (Some(Status::Unknown), None) => self.trim_and_continue(cache),
+            (Some(Status::Operational), _) => self.trim_and_continue(cache),
+            (Some(Status::Damaged), Some(mut group_size)) => {
+                let mut slice = self.springs.as_slice();
+
+                loop {
+                    match (slice, group_size) {
+                        ([], 0) => return u64::from(self.groups.len() == 1), // done
+                        ([], _) => return 0,                                 // invalid
+                        ([Status::Damaged, _rest @ ..], 0) => return 0,
+                        ([Status::Damaged, rest @ ..], _) => {
+                            slice = rest;
+                            group_size -= 1;
+                            continue;
+                        }
+                        ([Status::Operational, rest @ ..], 0) => {
+                            let new_springs = Vec::from(rest);
+                            let _ = std::mem::replace(&mut self.springs, new_springs);
+                            self.groups.remove(0);
+                            return self.possible_arrangements(cache);
+                        }
+                        ([Status::Operational, _rest @ ..], _) => return 0, // invalid
+                        ([Status::Unknown, rest @ ..], 0) => {
+                            let new_springs = Vec::from(rest);
+                            let _ = std::mem::replace(&mut self.springs, new_springs);
+                            self.groups.remove(0);
+                            return self.possible_arrangements(cache);
+                        }
+                        ([Status::Unknown, rest @ ..], _) => {
+                            slice = rest;
+                            group_size -= 1;
+                            continue;
+                        }
+                    };
+                }
+            }
+            (Some(Status::Unknown), Some(_)) => {
+                let new_row = self.replace(0, Status::Operational);
+                let new_springs = new_row.springs.clone();
+                let try_operational = new_row.possible_arrangements(cache);
+                cache.insert((new_springs, self.groups.clone()), try_operational);
+
+                let new_row = self.replace_owned(0, Status::Damaged);
+                let groups = new_row.groups.clone();
+                let new_springs = new_row.springs.clone();
+                let try_damaged = new_row.possible_arrangements(cache);
+                cache.insert((new_springs, groups), try_damaged);
+
+                try_operational + try_damaged
+            }
+        }
+    }
+
+    fn trim_and_continue(mut self, cache: &mut Cache) -> u64 {
+        self.springs.remove(0);
+        self.possible_arrangements(cache)
     }
 }
+
+type Cache = HashMap<(Vec<Status>, Vec<usize>), u64>;
 
 fn main() {
     // let input = include_str!("../../test_input.txt");
     let input = include_str!("../../input.txt");
 
     let rows: Vec<_> = input.lines().map(Row::parse).collect();
-    let part1: usize = rows.into_iter().map(Row::possible_arrangements).sum();
+
+    let mut cache = HashMap::<(Vec<Status>, Vec<usize>), u64>::new();
+    let part1: u64 = rows
+        .clone()
+        .into_iter()
+        .map(|row| row.possible_arrangements(&mut cache))
+        .sum();
     println!("part1 = {part1}");
+
+    let part2: u64 = rows
+        .into_iter()
+        .map(|row| row.unfold().possible_arrangements(&mut cache))
+        .sum();
+    println!("part2 = {part2}");
 }
