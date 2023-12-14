b9601ed0de89f53be923e8845deee9d45b75e5cd
Collin Styles <collingstyles@gmail.com>
2023-12-13 21:32:38 -0800

    Day 14, Rust: Part 2

--- day-14/rust/src/main.rs
+++ day-14/rust/src/main.rs
@@ -1,4 +1,6 @@
-#[derive(Debug, Copy, Clone, PartialEq, Eq)]
+use std::collections::HashMap;
+
+#[derive(Debug, Copy, Clone, PartialEq, Eq, Hash)]
 enum Tile {
     Empty,
     Round,
@@ -57,7 +59,102 @@ fn tilt_north(grid: &mut Grid) {
     }
 }
 
-fn load(grid: &Grid) -> usize {
+fn tilt_south(grid: &mut Grid) {
+    loop {
+        let mut rocks_moved = false;
+
+        for y in (0..grid.len()).rev() {
+            for x in (0..grid[0].len()).rev() {
+                let tile = grid[y][x];
+
+                if tile != Tile::Round {
+                    continue;
+                };
+
+                let down_y = y + 1;
+                if down_y >= grid.len() {
+                    continue;
+                };
+
+                let tile_below = grid[down_y][x];
+                if tile_below == Tile::Empty {
+                    grid[down_y][x] = Tile::Round;
+                    grid[y][x] = Tile::Empty;
+                    rocks_moved = true;
+                }
+            }
+        }
+
+        if !rocks_moved {
+            break;
+        }
+    }
+}
+
+fn tilt_west(grid: &mut Grid) {
+    loop {
+        let mut rocks_moved = false;
+
+        for x in 0..grid[0].len() {
+            for row in grid.iter_mut() {
+                let tile = row[x];
+
+                if tile != Tile::Round {
+                    continue;
+                };
+
+                let Some(left_x) = x.checked_sub(1) else {
+                    continue;
+                };
+
+                let left_tile = row[left_x];
+                if left_tile == Tile::Empty {
+                    row[left_x] = Tile::Round;
+                    row[x] = Tile::Empty;
+                    rocks_moved = true;
+                }
+            }
+        }
+
+        if !rocks_moved {
+            break;
+        }
+    }
+}
+
+fn tilt_east(grid: &mut Grid) {
+    loop {
+        let mut rocks_moved = false;
+
+        for x in 0..grid[0].len() {
+            for y in 0..grid.len() {
+                let tile = grid[y][x];
+
+                if tile != Tile::Round {
+                    continue;
+                };
+
+                let right_x = x + 1;
+                if right_x >= grid[0].len() {
+                    continue;
+                };
+
+                let right_tile = grid[y][right_x];
+                if right_tile == Tile::Empty {
+                    grid[y][right_x] = Tile::Round;
+                    grid[y][x] = Tile::Empty;
+                    rocks_moved = true;
+                }
+            }
+        }
+
+        if !rocks_moved {
+            break;
+        }
+    }
+}
+
+fn load(grid: Grid) -> usize {
     grid.iter()
         .rev()
         .enumerate()
@@ -75,12 +172,38 @@ fn main() {
         .map(|line| line.chars().map(Tile::parse).collect())
         .collect();
 
-    debug(&grid);
-    println!();
-    tilt_north(&mut grid);
-    debug(&grid);
+    let mut part1_grid = grid.clone();
+    tilt_north(&mut part1_grid);
+    println!("part1 = {}", load(part1_grid));
 
-    println!("part1 = {}", load(&grid));
+    let mut seen: HashMap<Grid, usize> = [(grid.clone(), 0)].into();
+    let mut cycle = 0;
+
+    let first_cycle_of_loop = loop {
+        cycle += 1;
+
+        tilt_north(&mut grid);
+        tilt_west(&mut grid);
+        tilt_south(&mut grid);
+        tilt_east(&mut grid);
+
+        if let Some(prev_round) = seen.insert(grid.clone(), cycle) {
+            break prev_round;
+        }
+    };
+
+    let cycle_length = cycle - first_cycle_of_loop;
+    let skip_factor = (1_000_000_000 - cycle) / cycle_length;
+    let leftover_cycles = 1_000_000_000 - cycle - cycle_length * skip_factor;
+
+    for _ in 0..leftover_cycles {
+        tilt_north(&mut grid);
+        tilt_west(&mut grid);
+        tilt_south(&mut grid);
+        tilt_east(&mut grid);
+    }
+
+    println!("part1 = {}", load(grid));
 }
 
 #[allow(dead_code)]
