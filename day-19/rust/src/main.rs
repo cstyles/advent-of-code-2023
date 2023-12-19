bd4f53b58f2342a159644bff01e3c791ed266f3a
Collin Styles <collingstyles@gmail.com>
2023-12-18 22:10:27 -0800

    Day 19, Rust: Part 2

--- day-19/rust/src/main.rs
+++ day-19/rust/src/main.rs
@@ -1,4 +1,5 @@
 use std::collections::HashMap;
+use std::ops::Range;
 
 #[derive(Debug, Copy, Clone)]
 enum Operation {
@@ -58,6 +59,20 @@ impl<'a> Rule<'a> {
         };
 
         op(category, self.value).then_some(self.result)
+    }
+
+    fn constraint(&self) -> Range<usize> {
+        match self.operation {
+            Operation::LessThan => 0..self.value as usize,
+            Operation::GreaterThan => (self.value as usize + 1)..4001,
+        }
+    }
+
+    fn reverse_constraint(&self) -> Range<usize> {
+        match self.operation {
+            Operation::LessThan => (self.value as usize)..4001,
+            Operation::GreaterThan => 0..self.value as usize + 1,
+        }
     }
 }
 
@@ -174,4 +189,91 @@ fn main() {
     }
 
     println!("part1 = {part1}");
+
+    let constraints = Constraints::default();
+    let part2 = attempt(constraints, &workflows, "in");
+    println!("part2 = {part2}");
+}
+
+fn attempt(
+    mut constraints: Constraints,
+    workflows: &HashMap<&str, Workflow<'_>>,
+    name: &str,
+) -> u64 {
+    // dbg!(name);
+    let workflow = workflows.get(name).unwrap();
+
+    let mut possible = 0;
+    for rule in workflow.rules.iter() {
+        let new_constraints = constraints.clone().merge(rule.var, rule.constraint());
+
+        let sub_possible = match rule.result {
+            Outcome::Accept => new_constraints.size(),
+            Outcome::Reject => 0,
+            Outcome::Redirect(name) => attempt(new_constraints, workflows, name),
+        };
+
+        possible += sub_possible;
+
+        constraints = constraints.merge(rule.var, rule.reverse_constraint());
+    }
+
+    let default_possible = match workflow.default {
+        Outcome::Accept => constraints.size(),
+        Outcome::Reject => 0,
+        Outcome::Redirect(name) => attempt(constraints, workflows, name),
+    };
+
+    possible + default_possible
+}
+
+#[derive(Debug, Clone)]
+struct Constraints {
+    x: Range<usize>,
+    m: Range<usize>,
+    a: Range<usize>,
+    s: Range<usize>,
+}
+
+impl Default for Constraints {
+    fn default() -> Self {
+        Self {
+            x: 1..4001,
+            m: 1..4001,
+            a: 1..4001,
+            s: 1..4001,
+        }
+    }
+}
+
+impl Constraints {
+    fn merge(self, var: char, constraint: Range<usize>) -> Self {
+        match var {
+            'x' => Self {
+                x: range_overlap(self.x, constraint),
+                ..self
+            },
+            'm' => Self {
+                m: range_overlap(self.m, constraint),
+                ..self
+            },
+            'a' => Self {
+                a: range_overlap(self.a, constraint),
+                ..self
+            },
+            's' => Self {
+                s: range_overlap(self.s, constraint),
+                ..self
+            },
+            _ => unreachable!("ugh"),
+        }
+    }
+
+    fn size(&self) -> u64 {
+        (self.x.len() * self.m.len() * self.a.len() * self.s.len()) as u64
+    }
+}
+
+fn range_overlap(a: Range<usize>, b: Range<usize>) -> Range<usize> {
+    a.start.max(b.start)..a.end.min(b.end)
 }
