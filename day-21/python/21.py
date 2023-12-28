4d19a29aaa0f290f57fd32fa29922b8aa602723a
Collin Styles <collingstyles@gmail.com>
2023-12-27 17:27:00 -0800

    Day 21, Python: Part 2

--- day-21/python/21.py
+++ day-21/python/21.py
@@ -5,6 +5,8 @@ from collections import deque
 from enum import Enum
 from typing import TypeAlias
 
+ACTUAL_STEPS = 26_501_365
+
 
 class Tile(Enum):
     Garden = 1
@@ -137,6 +139,38 @@ class Grid:
 
         return reachable_tiles
 
+    def distance_to_subgrid(self, gy: int, gx: int) -> int:
+        if gy == 0:
+            if gx == 0:
+                return 0  # starting subgrid
+            else:
+                return self.width() // 2 + self.width() * (abs(gx) - 1)
+        else:
+            if gx == 0:
+                return self.height() // 2 + self.height() * (abs(gy) - 1)
+            else:
+                return (
+                    self.width() // 2
+                    + self.height() // 2
+                    + self.width() * (abs(gx) - 1)
+                    + self.height() * (abs(gy) - 1)
+                )
+
+    def num_reachable_in_subgrid(self, gy: int, gx: int) -> int:
+        distance_to_here = self.distance_to_subgrid(gy, gx)
+
+        distances = map(
+            lambda x: x[1] + distance_to_here,
+            grid.distances_for_subgrid(gy, gx).items(),
+        )
+        return len(
+            [
+                distance
+                for distance in distances
+                if distance <= ACTUAL_STEPS and distance % 2 == 1
+            ]
+        )
+
 
 QueueItem: TypeAlias = tuple[Point, int]
 
@@ -162,6 +196,59 @@ def part1(grid: Grid):
     print(f"part1 = {part1}")
 
 
+def part2(grid: Grid):
+    x = (ACTUAL_STEPS - grid.width() // 2) // grid.width()
+    y = (ACTUAL_STEPS - grid.height() // 2) // grid.height()
+    assert x == y
+
+    up_cap = grid.num_reachable_in_subgrid(-y, 0)
+    down_cap = grid.num_reachable_in_subgrid(y, 0)
+    left_cap = grid.num_reachable_in_subgrid(0, -x)
+    right_cap = grid.num_reachable_in_subgrid(0, x)
+
+    up_left_small_diag = grid.num_reachable_in_subgrid(-y, -1)
+    up_left_big_diag = grid.num_reachable_in_subgrid(-y + 1, -1)
+    up_right_small_diag = grid.num_reachable_in_subgrid(-y, 1)
+    up_right_big_diag = grid.num_reachable_in_subgrid(-y + 1, 1)
+    down_left_small_diag = grid.num_reachable_in_subgrid(1, -x)
+    down_left_big_diag = grid.num_reachable_in_subgrid(1, -x + 1)
+    down_right_small_diag = grid.num_reachable_in_subgrid(1, x)
+    down_right_big_diag = grid.num_reachable_in_subgrid(1, x - 1)
+
+    small_diag_segments = y
+    big_diag_segments = small_diag_segments - 1
+
+    caps = up_cap + down_cap + left_cap + right_cap
+    big_diagonals = big_diag_segments * (
+        up_left_big_diag + up_right_big_diag + down_left_big_diag + down_right_big_diag
+    )
+    small_diagonals = small_diag_segments * (
+        up_left_small_diag
+        + up_right_small_diag
+        + down_left_small_diag
+        + down_right_small_diag
+    )
+    diagonals = big_diagonals + small_diagonals
+
+    inner_odd = grid.num_reachable_in_subgrid(0, 0)
+    inner_even = grid.num_reachable_in_subgrid(0, 1)
+
+    inner_total = inner_odd
+    ring = 1
+
+    while ring < x:  # or y
+        if ring % 2 == 0:
+            per_subgrid = inner_odd
+        else:
+            per_subgrid = inner_even
+
+        inner_total += ring * 4 * per_subgrid
+        ring += 1
+
+    part2 = caps + diagonals + inner_total
+    print(f"part2 = {part2}")
+
+
 if __name__ == "__main__":
     # with open("../test_input.txt", "r") as f:
     with open("../input.txt", "r") as f:
@@ -169,3 +256,4 @@ if __name__ == "__main__":
 
     grid = Grid(input)
     part1(grid)
+    part2(grid)
